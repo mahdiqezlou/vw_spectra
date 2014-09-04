540a950059fba240abd841e9966463703f069c27
Simeon Bird <spb@ias.edu>
2014-09-04 09:45:31 -0400

    Make a plot of the histogram of the median differences

--- make_pairs_plots.py
+++ make_pairs_plots.py
@@ -32,6 +32,7 @@ def plot_median_bar(hspecc, idnum, label):
     median = np.median(vw)
     plt.errorbar([median,], idnum, xerr = [[median - np.percentile(vw, 25),], [np.percentile(vw, 75)- median,]], fmt='o', label=label)
     plt.figure(1)
+    return median
 
 def plot_median_pair(haloname, snapnum, subhalopair, idnum=1):
     """Plot the median and quartiles for a pair"""
@@ -40,7 +41,7 @@ def plot_median_pair(haloname, snapnum, subhalopair, idnum=1):
     hspec = ps.VWPlotSpectra(snapnum, haloname, label=label, savefile=savefile)
     plt.figure(1)
     hspec.plot_vel_width("Si", 2, color=colors[idnum], ls = "--")
-    plot_median_bar(hspec, idnum, label)
+    return plot_median_bar(hspec, idnum, label)
 
 sim = 5
 snap = 3
@@ -48,8 +49,9 @@ halo = myname.get_name(sim, True, box=10)
 #Load from a save file only
 total = ps.VWPlotSpectra(snap, halo, label="Total")
 total.plot_vel_width("Si", 2, color="black", ls = "-")
-plot_median_bar(total, 0, "Total")
+tot_median = plot_median_bar(total, 0, "Total")
 
+medians = []
 #Get the subhalo list
 subs=subfindhdf.SubFindHDF5(halo, snap)
 #In solar masses
@@ -58,7 +60,7 @@ loc= 2.4
 plt.text(10, loc, "Halo mass: (1e10 Msun)")
 height=1
 for pair in ([1,4], [835, 837], [1556, 1558]):
-    plot_median_pair(halo, snap, pair, height)
+    medians.append(plot_median_pair(halo, snap, pair, height))
     height+=1
     for n in pair:
         loc-=0.1
@@ -69,7 +71,7 @@ snap = 4
 subs=subfindhdf.SubFindHDF5(halo, snap)
 sub_mass=np.array(subs.get_sub("SubhaloMass"))
 for pair in ([2531,2532], [3240, 3241]):
-    plot_median_pair(halo, snap, pair, height)
+    medians.append(plot_median_pair(halo, snap, pair, height))
     height+=1
     for n in pair:
         loc-=0.1
@@ -80,7 +82,7 @@ snap = 5
 subs=subfindhdf.SubFindHDF5(halo, snap)
 sub_mass=np.array(subs.get_sub("SubhaloMass"))
 for pair in ([3775, 3776],):
-    plot_median_pair(halo, snap, pair, height)
+    medians.append(plot_median_pair(halo, snap, pair, height))
     height+=1
     for n in pair:
         loc-=0.1
@@ -94,3 +96,11 @@ plt.figure(2)
 plt.legend()
 plt.ylim(-0.1, height+1)
 save_figure(outdir+"pairs_scatter")
+plt.clf()
+
+table=np.logspace(np.log10(np.min(medians)),np.log10(np.max(medians)),4)
+center = np.array([(table[i]+table[i+1])/2. for i in range(0,np.size(table)-1)])
+nn = np.histogram(medians,table)[0]
+plt.errorbar(center,nn,xerr=[center-table[:-1],table[1:]-center],fmt='.', color="black")
+save_figure(outdir+"pairs_pdf")
+plt.clf()
