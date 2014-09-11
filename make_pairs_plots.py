df843e40e9cef1225d2c44e80cf30bce1066a042
Simeon Bird <spb@ias.edu>
2014-09-11 18:46:24 -0400

    Plot big box

--- make_pairs_plots.py
+++ make_pairs_plots.py
@@ -23,7 +23,7 @@ def pr_num(num,rnd=2):
     """Return a string rep of a number"""
     return str(np.round(num,rnd))
 
-colors = ["black", "red", "blue", "green", "grey", "brown", "pink", "orange", "cyan"]
+colors = ["black", "red", "blue", "green", "grey", "brown", "pink", "orange", "cyan", "magenta"]
 
 def plot_median_bar(hspecc, idnum, label):
     """Plot median with error bars for velocity widths"""
@@ -40,15 +40,64 @@ def plot_median_pair(haloname, snapnum, subhalopair, idnum=1):
     savefile = "subhalo_spectra_"+"".join(map(str,subhalopair))+".hdf5"
     hspec = ps.VWPlotSpectra(snapnum, haloname, label=label, savefile=savefile)
     plt.figure(1)
-    hspec.plot_vel_width("Si", 2, color=colors[idnum], ls = "--")
+    hspec.plot_vel_width("Si", 2, color=colors[idnum % np.size(colors)], ls = "--")
     return plot_median_bar(hspec, idnum, label)
 
+#Do big box
+base = "/home/spb/data/Illustris/"
+savedir = "/home/spb/data/Illustris/"
+pairs63 = np.loadtxt("pairs063.txt")
+pairs = zip(pairs63[:,0], pairs63[:,1])
+
+medians = []
+height = 1
+for pair in pairs:
+    medians.append(plot_median_pair(base, 63, pair, height))
+    height+=1
+
+pairs68 = np.loadtxt("pairs068.txt")
+pairs = zip(pairs68[:,0], pairs68[:,1])
+
+for pair in pairs:
+    medians.append(plot_median_pair(base, 68, pair, height))
+    height+=1
+
+
+#My simulation
 sim = 5
 snap = 3
 halo = myname.get_name(sim, True, box=10)
 #Load from a save file only
 total = ps.VWPlotSpectra(snap, halo, label="Total")
 total.plot_vel_width("Si", 2, color="black", ls = "-")
+
+plt.xlim(10,4000)
+# plt.legend()
+save_figure(outdir+"illus_pairs_vw")
+
+tot_median = plot_median_bar(total, 0, "Total")
+# plt.legend()
+plt.figure(2)
+plt.ylim(-0.1, height+1)
+save_figure(outdir+"illus_pairs_scatter")
+plt.clf()
+
+table=np.logspace(np.log10(np.min(medians)),np.log10(np.max(medians)),10)
+center = np.array([(table[i]+table[i+1])/2. for i in range(0,np.size(table)-1)])
+nn = np.histogram(medians,table)[0]
+plt.semilogx(center,nn, ls="-", color="red")
+total.plot_vel_width("Si", 2, color="black", ls = "-")
+plt.xlim(10,100)
+save_figure(outdir+"illus_pairs_pdf")
+plt.clf()
+
+#My simulation
+sim = 5
+snap = 3
+halo = myname.get_name(sim, True, box=10)
+plt.figure(2)
+total.plot_vel_width("Si", 2, color="black", ls = "-")
+plt.figure(1)
 tot_median = plot_median_bar(total, 0, "Total")
 
 medians = []
